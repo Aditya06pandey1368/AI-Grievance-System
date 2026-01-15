6823cdf9f62701d995aec197713063f5dec52664
Aditya Pandey <aditya06pandey1368@gmail.com>
2026-01-15 11:33:33 +0530

    Remove department creation logic from createDept.js

--- server/createDept.js
+++ server/createDept.js
@@ -1,13 +1,1 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import Department from './src/models/Department.model.js';
-dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-   const dept = await Department.create({ 
-       name: "Roads & Highways", 
-       code: "DEPT_ROADS" 
-   });
-   console.log("✅ Department Created:", dept._id); // <--- COPY THIS ID
-   process.exit();
-});
